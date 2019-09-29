fac6d53ce71b6fbe44d36bcb9ce3cf90bc6330fe
arungeorgeexperion <arun.grorge@experionglobal.com>
2019-09-29 23:40:45 +0530

    Github profile checking app using angular and typescript

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { SearchComponent } from './search/search.component';
+import { NodataComponent } from './nodata/nodata.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    SearchComponent,
+    NodataComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import {  HttpClient } from '@angular/common/http';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+  search: string;  // Entered username
+  profile: object; // object storing all the details gathered
+  loaderRequirement: boolean;  // flag to identify the places where loader
+  errorMessage: boolean;  // flag to check whether to display error-message or not
+  showButton: boolean;  // finding contexts where the button is to be displayed
+  setMargin: number;  // variable to set margin of the profile card from the search bar 
+
+  constructor(private httpClient: HttpClient, private toast: ToastrService) { }
+  ngOnInit() {
+    this.errorMessage = false;
+    this.setMargin = 0;
+    this.search = "";
+    this.showButton = true;
+    this.loaderRequirement = false;
+  }
+  find() {
+    this.showButton = false;
+    this.loaderRequirement = true;
+    this.errorMessage = false;
+    delete this.profile;
+    if(localStorage.getItem(this.search)) // checking if the searching profile is already there in local storage
+    {
+      console.log("entered");
+      this.setMargin = 1;
+      this.profile = JSON.parse(localStorage.getItem(this.search));
+      this.loaderRequirement = false;
+    }
+    else // If username is entered for the first time
+    {
+    this.httpClient.get(`https://api.github.com/users/` + this.search + `?access_token=b3722c2825a4d6957432e11ea6956c50928f119c`)
+      .subscribe((result) => {
+        this.setMargin = 1;
+        this.loaderRequirement = false;
+        this.profile = result;
+        localStorage.setItem(this.search, JSON.stringify(this.profile));  // Storing the profile to local storage 
+      },
+        (error) => {
+          delete this.profile;
+          this.errorMessage = true;
+          this.loaderRequirement = false;
+          if (error.status == 404) {
+            this.toast.error('Name is incorrect', 'Please check the name entered', {
+              timeOut: 3000
+            })
+          }
+          else if (error.status == 403) {
+            this.toast.error('Sorry,Cannot find the id', 'Try at a later time', {
+              timeOut: 3000
+            })
+          }
+        });
+  }}
+  settingButton() {
+    this.showButton = true;
+    this.setMargin = 5;
+  }
+}
